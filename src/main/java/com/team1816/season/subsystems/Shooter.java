05269efc6c61aaee1ffd613a34466fce4906ea5a
ishwaqo8120 <ishmomar14@gmail.com>
2026-01-21 15:03:28 -0600

    Added motors and "BASIC" findAngle and findIncline methods. (FEEL FREE TO CHANGE ANYTHING HERE!!)

--- src/main/java/com/team1816/season/subsystems/Shooter.java
+++ src/main/java/com/team1816/season/subsystems/Shooter.java
@@ -5,10 +5,12 @@ import com.ctre.phoenix6.controls.VoltageOut;
 import com.team1816.lib.hardware.components.motor.IMotor;
 import com.team1816.lib.subsystems.ITestableSubsystem;
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static com.team1816.lib.Singleton.factory;
+
 public class Shooter extends SubsystemBase implements ITestableSubsystem {
     /*TODO: 1. create "findAngle" method
     2. Code in blind spot
@@ -20,20 +22,31 @@ public class Shooter extends SubsystemBase implements ITestableSubsystem {
     8. Beam break implementation
     9. 17.5:1 rot:cycle
     10. 1:22.5  rot: degree
+    11. Added subsystem to yaml ✓(for now)
     */
     String NAME = "shooter";
 
     private double GEAR_RATIO = 1.0;
 
     private double wantedAngle = 0.0;
+    private double wantedIncline = 0;
 
-    private final IMotor shooterMotor = (IMotor) factory.getDevice(NAME, "shooterMotor");
+    private final IMotor turretMotor = (IMotor) factory.getDevice(NAME, "turretMotor");
+    private final IMotor shooterMotorLeader = (IMotor) factory.getDevice(NAME, "shooterMotorTop");
+    private final IMotor shooterMotorFollower = (IMotor) factory.getDevice(NAME, "shooterMotorBottom");
+    private final IMotor gatekeeperMotor = (IMotor) factory.getDevice(NAME, "gatekeeperMotor");
+
+    private final Servo servoLeader = (Servo) factory.getDevice(NAME, "servoLeader");
+    private final Servo servoFollower = (Servo) factory.getDevice(NAME, "servoFollower");
+
+
     private VoltageOut voltageControl = new VoltageOut(0);
     private PositionVoltage positionControl = new PositionVoltage(0);
     private SHOOTER_STATE wantedState = SHOOTER_STATE.SHOOTER_IDLE;
 
     public double currentVoltage = 0;
     public double currentPosition = 0;
+
 
     public enum SHOOTER_STATE {
         SHOOTER_TO_0,
@@ -42,6 +55,11 @@ public class Shooter extends SubsystemBase implements ITestableSubsystem {
         SHOOTER_ROTATE_LEFT,
         SHOOTER_ROTATE_RIGHT,
         SHOOTER_IDLE
+    }
+    public enum INCLINE_STATE {
+        LOW,
+        MID,
+        HIGH
     }
 
     public void periodic() {
@@ -53,53 +71,67 @@ public class Shooter extends SubsystemBase implements ITestableSubsystem {
         this.wantedState = state;
     }
 
-    public void setWantedState(SHOOTER_STATE state, double angle) {
+    public void setWantedState(SHOOTER_STATE state, double angle, double incline) {
         this.wantedState = state;
         this.wantedAngle = angle;
+        this.wantedIncline = incline;
+
     }
 
     @Override
     public void readFromHardware() {
-        currentPosition = shooterMotor.getMotorPosition();
+        currentPosition = turretMotor.getMotorPosition();
         currentVoltage = 0;
     }
 
     private void applyState() {
         switch (wantedState) {
             case SHOOTER_TO_0:
-                setShooterAngle(0);
+                setTurretAngle(0);
                 break;
             case SHOOTER_TO_180:
-                setShooterAngle(180);
+                setTurretAngle(180);
                 break;
             case SHOOTER_TO_ANGLE:
-                setShooterAngle(wantedAngle);
+                setTurretAngle(wantedAngle);
                 break;
             case SHOOTER_ROTATE_LEFT:
-                setShooterSpeed(1);
+                setTurretSpeed(1);
                 break;
             case SHOOTER_ROTATE_RIGHT:
-                setShooterSpeed(-1);
+                setTurretSpeed(-1);
                 break;
             case SHOOTER_IDLE:
             default:
-                setShooterSpeed(0);
+                setTurretSpeed(0);
                 break;
+
         }
     }
+    public double findAngle () {
 
-    public void setShooterSpeed(double wantedSpeed) {
+        return wantedAngle;
+    }
+    public double findIncline () {
+
+        return wantedIncline;
+    }
+
+    public void setTurretSpeed(double wantedSpeed) {
         double output = MathUtil.clamp(wantedSpeed, -12.0, 12.0);
 
         SmartDashboard.putNumber("Shooter Voltage", output);
 
-        shooterMotor.setControl(voltageControl.withOutput(output));
+        turretMotor.setControl(voltageControl.withOutput(output));
     }
 
-    public void setShooterAngle(double wantedAngle) {
+    public void setTurretAngle(double wantedAngle) {
         double rotations = (wantedAngle / 360.0) * GEAR_RATIO;
 
-        shooterMotor.setControl(positionControl.withPosition(rotations));
+        turretMotor.setControl(positionControl.withPosition(rotations));
+    }
+    public void setShooterIncline(double wantedIncline){
+        double incline = wantedIncline;
     }
 
 }
