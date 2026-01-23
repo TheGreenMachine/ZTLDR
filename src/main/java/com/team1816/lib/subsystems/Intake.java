48af6f0375efd85155d76d672b83d76abc79fa01
Noah <71520770+DogeDoge17@users.noreply.github.com>
2026-01-22 18:03:01 -0600

    simple intake

--- src/main/java/com/team1816/lib/subsystems/Intake.java
+++ src/main/java/com/team1816/lib/subsystems/Intake.java
@@ -1,5 +1,6 @@
 package com.team1816.lib.subsystems;
 
+import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.team1816.lib.hardware.components.motor.IMotor;
@@ -7,14 +8,19 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import javax.swing.text.Position;
+
 import static com.team1816.lib.Singleton.factory;
 
 public class Intake extends SubsystemBase implements ITestableSubsystem {
+    private static final double GEAR_RATIO = 1;
     String NAME = "intake";
 
     private final IMotor intake = (IMotor) factory.getDevice(NAME, "intakeMotor");
+    private final IMotor flipper = (IMotor) factory.getDevice(NAME, "flipperMotor");
     //private VoltageOut voltageControl = new VoltageOut(0);
     private VelocityVoltage velocityControl = new VelocityVoltage(0);
+    private PositionVoltage positionControl = new PositionVoltage(0);
     private INTAKE_STATE wantedState = INTAKE_STATE.INTAKE_IDLE;
 
     public double currentVoltage = 0;
@@ -45,15 +51,24 @@ public class Intake extends SubsystemBase implements ITestableSubsystem {
         switch (wantedState) {
             case INTAKE_IN:
                 setTurretSpeed(10);
+                setFlipperAngle(225);
                 break;
             case INTAKE_OUT:
                 setTurretSpeed(-10);
+                setFlipperAngle(225);
                 break;
             case INTAKE_IDLE:
             default:
                 setTurretSpeed(0);
+                setFlipperAngle(45);
                 break;
         }
+    }
+
+    public void setFlipperAngle(double wantedAngle) {
+        double rotations = (wantedAngle / 360.0) * GEAR_RATIO;
+
+        flipper.setControl(positionControl.withPosition(rotations));
     }
 
     public void setTurretSpeed(double wantedSpeed) {
