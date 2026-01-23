7d9425872e1e79c335d3ca988f69cb18847d7a2f
Ethan <137455719+EndigoSkull@users.noreply.github.com>
2026-01-22 19:39:56 -0600

    Added initial climber logic according to initial prototype, there is no coordination collision detection

--- src/main/java/com/team1816/season/subsystems/Climber.java
+++ src/main/java/com/team1816/season/subsystems/Climber.java
@@ -1,6 +1,6 @@
 package com.team1816.season.subsystems;
 
-import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.controls.PositionVoltage;
 import com.team1816.lib.hardware.components.motor.IMotor;
 import com.team1816.lib.subsystems.ITestableSubsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,10 +10,29 @@ import static com.team1816.lib.Singleton.factory;
 public class Climber extends SubsystemBase implements ITestableSubsystem {
 
     public static final String NAME = "climber";
-    private final IMotor climberMotor = (IMotor)factory.getDevice(NAME, "climberMotor");
-    private double curPosition;
-    private CLIMBER_STATE wantedState = CLIMBER_STATE.REST;
-    VelocityVoltage climReq = new VelocityVoltage(0);
+    private boolean wantedStateChanged;
+
+    private CLIMBER_WANTED_STATE wantedState = CLIMBER_WANTED_STATE.STOW;
+
+    private final IMotor climbMotor = (IMotor)factory.getDevice(NAME, "climberMotor");
+    PositionVoltage climbReq = new PositionVoltage(0);
+    private final IMotor stowerMotor = (IMotor)factory.getDevice(NAME, "stowMotor");
+    PositionVoltage stowerReq = new PositionVoltage(0);
+
+    private double climbCurrentPosition;
+    private double stowerCurrentPosition;
+
+    private double climbPositionStow = factory.getConstant(NAME, "climbPositionStow", 0);
+    private double climbPositionReady = factory.getConstant(NAME, "climbPositionUnstow", 0);
+    private double climbPositionL1 = factory.getConstant(NAME, "climbPositionL1", 0);
+    private double climbPositionL2 = factory.getConstant(NAME, "climbPositionL2", 0);
+    private double climbPositionL3 = factory.getConstant(NAME, "climbPositionL3", 0);
+
+    private double stowerPositionStow = factory.getConstant(NAME, "stowerPositionStow", 0);
+    private double stowerPositionUnstow = factory.getConstant(NAME, "stowerPositionUnstow", 0);
+
+    private double climbReadyPositionTolerance = 0.01;
+    private double stowerStowPositionTolerance = 0.01;
 
     @Override
     public void periodic() {
@@ -23,27 +42,51 @@ public class Climber extends SubsystemBase implements ITestableSubsystem {
 
     @Override
     public void readFromHardware() {
-        curPosition = climberMotor.getMotorPosition();
+        climbCurrentPosition = climbMotor.getMotorPosition();
+        stowerCurrentPosition = stowerMotor.getMotorPosition();
     }
 
     private void applyState() {
         switch (wantedState) {
-            case REST -> climberMotor.setControl(climReq.withVelocity(1));
-            case L1 -> climberMotor.setControl(climReq.withVelocity(1));
-            case L2 -> climberMotor.setControl(climReq.withVelocity(1));
-            case L3 -> climberMotor.setControl(climReq.withVelocity(1));
-
+            case STOW -> coordinateControlRequest(climbPositionStow, stowerPositionStow);
+            case READY -> coordinateControlRequest(climbPositionReady, stowerPositionUnstow);
+            case L1 -> coordinateControlRequest(climbPositionL1, stowerPositionUnstow);
+            case L2 -> coordinateControlRequest(climbPositionL2, stowerPositionUnstow);
+            case L3 -> coordinateControlRequest(climbPositionL3, stowerPositionUnstow);
         }
     }
 
-    public enum CLIMBER_STATE {
-        REST,
+    private void coordinateControlRequest(double climbMotorReq, double stowerMotorReq){
+/*        if (Math.abs(climbCurrentPosition - climbPositionStow) <= climbReadyPositionTolerance){
+            if (stowerReq.getControlInfo())
+            stowerMotor.setControl(stowerReq.withPosition(stowerMotorReq));
+
+        } else {
+            climbMotor.setControl(climbReq.withPosition(climbMotorReq));
+        }*/
+
+        climbMotor.setControl(climbReq.withPosition(climbMotorReq));
+        stowerMotor.setControl(stowerReq.withPosition(stowerMotorReq));
+    }
+
+    public enum CLIMBER_WANTED_STATE {
+        STOW,
+        READY,
         L1,
         L2,
         L3
     }
 
-    public void setWantedState(CLIMBER_STATE state) {
-        wantedState = state;
+    public enum CLIMBER_SYSTEM_STATE {
+        STOW,
+        READY,
+        L1,
+        L2,
+        L3
+    }
+
+    public void setWantedState(CLIMBER_WANTED_STATE wantedState) {
+        wantedStateChanged = true;
+        this.wantedState = wantedState;
     }
 }
