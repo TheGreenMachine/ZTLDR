db5e905a7d08205c74fd2e5061274d136d7df45c
Ethan <137455719+EndigoSkull@users.noreply.github.com>
2026-01-30 14:20:15 -0600

    Climber Mech2d Ready For Sim

--- src/main/java/com/team1816/season/subsystems/Climber.java
+++ src/main/java/com/team1816/season/subsystems/Climber.java
@@ -3,6 +3,10 @@ package com.team1816.season.subsystems;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.team1816.lib.hardware.components.motor.IMotor;
 import com.team1816.lib.subsystems.ITestableSubsystem;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static com.team1816.lib.Singleton.factory;
@@ -14,10 +18,22 @@ public class Climber extends SubsystemBase implements ITestableSubsystem {
 
     private CLIMBER_WANTED_STATE wantedState = CLIMBER_WANTED_STATE.STOW;
 
+    public final Mechanism2d climberMech = new Mechanism2d(6, 6.5);
+    public final MechanismRoot2d climberMechRoot = climberMech.getRoot("root", 1, 1.5);
+
+    private double firstArmAngleDeg = 0;
+    public final MechanismLigament2d climberMechFirstArm = climberMechRoot.append(new MechanismLigament2d("FirstArm", 3.5, firstArmAngleDeg));
+    private double secondArmAngleDeg = -110;
+    public final MechanismLigament2d climberMechSecondArm = climberMechFirstArm.append(new MechanismLigament2d("SecondArm", 1.5, secondArmAngleDeg));
+    public final MechanismLigament2d climberMechThirdArm = climberMechSecondArm.append(new MechanismLigament2d("ThirdArm", 2, 50-secondArmAngleDeg));
+
     private final IMotor climbMotor = (IMotor)factory.getDevice(NAME, "climberMotor");
     PositionVoltage climbReq = new PositionVoltage(0);
-    private final IMotor stowerMotor = (IMotor)factory.getDevice(NAME, "stowMotor");
+    private final IMotor stowerMotor = (IMotor)factory.getDevice(NAME, "stowerMotor");
     PositionVoltage stowerReq = new PositionVoltage(0);
+
+    double lastClimberMotorRequest = -10000;
+    double lastStowerMotorRequest = -10000;
 
     private double climbCurrentPosition;
     private double stowerCurrentPosition;
@@ -34,10 +50,16 @@ public class Climber extends SubsystemBase implements ITestableSubsystem {
     private double climbReadyPositionTolerance = 0.01;
     private double stowerStowPositionTolerance = 0.01;
 
+    public Climber () {
+        super();
+        SmartDashboard.putData("ClimbMechanism2d", climberMech);
+    }
+
     @Override
     public void periodic() {
         readFromHardware();
         applyState();
+        outputToSmartDashboard();
     }
 
     @Override
@@ -57,16 +79,41 @@ public class Climber extends SubsystemBase implements ITestableSubsystem {
     }
 
     private void coordinateControlRequest(double climbMotorReq, double stowerMotorReq){
-/*        if (Math.abs(climbCurrentPosition - climbPositionStow) <= climbReadyPositionTolerance){
-            if (stowerReq.getControlInfo())
-            stowerMotor.setControl(stowerReq.withPosition(stowerMotorReq));
-
-        } else {
+        if (lastClimberMotorRequest != climbMotorReq) {
             climbMotor.setControl(climbReq.withPosition(climbMotorReq));
-        }*/
+            lastClimberMotorRequest = climbMotorReq;
+        }
+        if (lastStowerMotorRequest != stowerMotorReq) {
+            stowerMotor.setControl(stowerReq.withPosition(stowerMotorReq));
+            lastStowerMotorRequest = stowerMotorReq;
+        }
+    }
 
-        climbMotor.setControl(climbReq.withPosition(climbMotorReq));
-        stowerMotor.setControl(stowerReq.withPosition(stowerMotorReq));
+    private void offsetClimbMotor(double offsetAmount){
+        switch (wantedState) {
+            case STOW -> climbPositionStow += offsetAmount;
+            case READY -> climbPositionReady += offsetAmount;
+            case L1 -> climbPositionL1 += offsetAmount;
+            case L2 -> climbPositionL2 += offsetAmount;
+            case L3 -> climbPositionL3 += offsetAmount;
+        }
+    }
+
+    private void offsetStowerMotor(double offsetAmount){
+        switch (wantedState) {
+            case STOW -> stowerPositionStow += offsetAmount;
+            case READY -> stowerPositionUnstow += offsetAmount;
+/*            case L1 -> stowerPositionUnstow += offsetAmount;  //removed because this probably shouldn't be offset
+            case L2 -> stowerPositionUnstow += offsetAmount;
+            case L3 -> stowerPositionUnstow += offsetAmount;*/
+        }
+    }
+
+    private void outputToSmartDashboard(){
+        if (climberMechFirstArm.getAngle() != firstArmAngleDeg+90*(Math.abs(stowerCurrentPosition-stowerPositionStow)/Math.abs(stowerPositionUnstow-stowerPositionStow))) {
+            climberMechFirstArm.setAngle(firstArmAngleDeg + 90 * (Math.abs(stowerCurrentPosition - stowerPositionStow) / Math.abs(stowerPositionUnstow - stowerPositionStow)));
+            SmartDashboard.putData("ClimbMechanism2d", climberMech);
+        }
     }
 
     public enum CLIMBER_WANTED_STATE {
