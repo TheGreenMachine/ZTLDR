db48172da00e0057e0d6c82fa68fdec05c1f467e
AlexanderBecker13 <144349466+AlexanderBecker13@users.noreply.github.com>
2026-01-29 19:52:50 -0600

    cleaned up code

--- src/main/java/com/team1816/season/subsystems/RobotPositionValues.java
+++ src/main/java/com/team1816/season/subsystems/RobotPositionValues.java
@@ -9,16 +9,6 @@ import edu.wpi.first.wpilibj.DriverStation;
 
 
 public class RobotPositionValues {
-    //    public static Translation2d getDistanceToBlueHub(){
-//        var transform = new Transform2d(RobotState.swerveDriveState.Pose, new Pose2d(4.6228,3.8608,Rotation2d.kZero));
-//        var BlueDistance = transform.getTranslation();
-//        return BlueDistance;
-//    }
-//    public static Translation2d getDistanceToRedHub(){
-//        var transform = new Transform2d(RobotState.swerveDriveState.Pose, new Pose2d(11.915394,3.8608,Rotation2d.kZero));
-//        var RED_TRANSFORM = transform.getTranslation();
-//        return RED_TRANSFORM;
-//    }
     public static Translation2d getDistance() {
         if (DriverStation.getAlliance().get() == DriverStation.Alliance.Blue) {
             var transform = new Transform2d(RobotState.swerveDriveState.Pose, new Pose2d(4.6228, 3.8608, Rotation2d.kZero));
