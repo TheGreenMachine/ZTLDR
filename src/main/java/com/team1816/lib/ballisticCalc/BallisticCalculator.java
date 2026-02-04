b71c9c830c196077069b26a1b6ce0390f926ab38
ishwaqo8120 <ishmomar14@gmail.com>
2026-02-04 12:43:35 -0600

    Initial Scaffolding of the BallisticCalc code

---
+++ src/main/java/com/team1816/lib/ballisticCalc/BallisticCalculator.java
@@ -0,0 +1,27 @@
+package com.team1816.lib.ballisticCalc;
+
+/**
+I know this isn't what it'll actually be structured, but this was the ultimate basis of what got need from the night of 1/03.
+ (I would recommend worrying about the initialAngle & initialVelocity later and just starting with start/end pose(will change as the robot moves) & distance)
+ There's some code in the .md file + our initial implementation of distance.
+ */
+public class BallisticCalculator {
+    public double getStartPose(double pose) {
+        return pose;
+    }
+    public double getEndPose(double pose) {
+        return pose;
+    }
+
+    public double getLaunchAngle(double launchAngle) {
+        return launchAngle;
+    }
+
+    public double getInitialVelocity(double initialVelocity) {
+        return initialVelocity;
+    }
+
+
+
+
+}
---
+++ src/main/java/com/team1816/lib/ballisticCalc/BallisticConstraints.java
@@ -0,0 +1,7 @@
+package com.team1816.lib.ballisticCalc;
+
+public class BallisticConstraints {
+    /**
+     * This will be where the turret and incline restraints will be. DO NOT send the motor to anything, these will all ultimately be getters.
+     */
+}
