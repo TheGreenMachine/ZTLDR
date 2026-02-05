c97f04efcb69034b45dda2555325ef5a0b3b791f
AlexanderBecker13 <144349466+AlexanderBecker13@users.noreply.github.com>
2026-02-04 19:42:08 -0600

    got our new ballistic library up and running

--- src/main/java/com/team1816/lib/ballisticCalc/BallisticConstraints.java
+++ src/main/java/com/team1816/lib/ballisticCalc/BallisticConstraints.java
@@ -4,4 +4,9 @@ public class BallisticConstraints {
     /**
      * This will be where the turret and incline restraints will be. DO NOT send the motor to anything, these will all ultimately be getters.
      */
+    private double enterAngle;
+    public double getEnterAngle (double enterAngle) {
+        this.enterAngle = enterAngle;
+        return enterAngle;
+    }
 }
