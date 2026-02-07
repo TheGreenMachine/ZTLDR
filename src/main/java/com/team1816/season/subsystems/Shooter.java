44e85e515b19d31766142538faee06e2a387c274
Ethan <137455719+EndigoSkull@users.noreply.github.com>
2026-02-07 13:26:12 -0600

    Streamlined the shooter class and related for clarity

--- src/main/java/com/team1816/season/subsystems/Shooter.java
+++ src/main/java/com/team1816/season/subsystems/Shooter.java
@@ -1,7 +1,7 @@
 package com.team1816.season.subsystems;
 
 import com.ctre.phoenix6.controls.PositionVoltage;
-import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.team1816.lib.BaseRobotState;
 import com.team1816.lib.ballisticCalc.BallisticCalculator;
 import com.team1816.lib.ballisticCalc.BallisticSolution;
@@ -9,33 +9,49 @@ import com.team1816.lib.hardware.components.motor.IMotor;
 import com.team1816.lib.subsystems.ITestableSubsystem;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static com.team1816.lib.Singleton.factory;
 public class Shooter extends SubsystemBase implements ITestableSubsystem {
+
+    //CLASS
     String NAME = "shooter";
 
-    private double GEAR_RATIO = 1.0;
+    private SHOOTER_STATE wantedState = SHOOTER_STATE.IDLE;
 
-    private double wantedAngle = 0.0;
-
+    //MOTORS
     private final IMotor topLaunchMotor = (IMotor) factory.getDevice(NAME, "topLaunchMotor");
     private final IMotor bottomLaunchMotor = (IMotor) factory.getDevice(NAME, "bottomLaunchMotor");
     private final IMotor launchAngleMotor = (IMotor) factory.getDevice(NAME, "launchAngleMotor");
     private final IMotor rotationAngleMotor = (IMotor) factory.getDevice(NAME, "rotationAngleMotor");
 
-    private VoltageOut voltageControl = new VoltageOut(0);
+    private VelocityVoltage velocityControl = new VelocityVoltage(0);
     private PositionVoltage positionControl = new PositionVoltage(0);
-    private SHOOTER_STATE wantedState = SHOOTER_STATE.AUTOMATIC;
 
+    //AUTO AIM
     private BallisticCalculator ballisticCalculator = new BallisticCalculator();
-    private double targetX;
-    private Translation3d targetTranslation;
+    private AUTO_AIM_TARGETS currentTarget = AUTO_AIM_TARGETS.BLUE_HUB;
+    private static final Translation3d SHOOTER_OFFSET = new Translation3d(0, 0, 22); //TODO WHEN PHYSICAL SUBSYSTEM EXISTS, set this.
 
-    public double currentVoltage = 0;
-    public double currentPosition = 0;
+    //CONSTANTS
+    private static final double MOTOR_ROTATIONS_PER_LAUNCH_ANGLE_DEGREE = 3.5/1; //TODO WHEN PHYSICAL SUBSYSTEM EXISTS, set this.
+    private static final double MOTOR_ROTATIONS_PER_ROTATION_ANGLE_DEGREE = 3.5/1; //TODO WHEN PHYSICAL SUBSYSTEM EXISTS, set this.
+
+    public enum AUTO_AIM_TARGETS{
+        BLUE_HUB(new Translation3d(4.6228, 3.8608, 40)),
+        RED_HUB(new Translation3d(11.915394, 3.8608, 40));
+
+        private Translation3d position;
+
+        AUTO_AIM_TARGETS (Translation3d position){
+            this.position = position;
+        }
+
+        public Translation3d getPosition(){
+            return position;
+        }
+    }
 
     public enum SHOOTER_STATE {
         DISTANCE_ONE(45, 0, 10),
@@ -71,19 +87,8 @@ public class Shooter extends SubsystemBase implements ITestableSubsystem {
         applyState();
     }
 
-    public void setWantedState(SHOOTER_STATE state) {
-        this.wantedState = state;
-    }
-
-    public void setWantedState(SHOOTER_STATE state, double angle) {
-        this.wantedState = state;
-        this.wantedAngle = angle;
-    }
-
     @Override
     public void readFromHardware() {
-        currentPosition = rotationAngleMotor.getMotorPosition();
-        currentVoltage = 0;
     }
 
     private void applyState() {
@@ -92,25 +97,17 @@ public class Shooter extends SubsystemBase implements ITestableSubsystem {
         double launchVelocity = wantedState.getLaunchVelocity();
 
         if (wantedState == SHOOTER_STATE.AUTOMATIC) {
-            // TODO: figure out launcher z value
-            Translation3d launcherTranslation = new Translation3d(BaseRobotState.swerveDriveState.Pose.getX(), BaseRobotState.swerveDriveState.Pose.getY(), 22);
-
+            Translation3d launcherTranslation = new Translation3d(BaseRobotState.swerveDriveState.Pose.getX(), BaseRobotState.swerveDriveState.Pose.getY(), 0).plus(SHOOTER_OFFSET);
             // TODO: figure out hub z value
-            if (DriverStation.getAlliance().isPresent() && DriverStation.getAlliance().get() == DriverStation.Alliance.Blue) {
-                targetX = 4.6228;
-            } else {
-                targetX = 11.915394;
-            }
-            targetTranslation = new Translation3d(targetX, 03.8608, 40);
-            BallisticSolution ballisticSolution = ballisticCalculator.getBallisticSolution(launcherTranslation, targetTranslation, 10);
+            BallisticSolution ballisticSolution = ballisticCalculator.getBallisticSolution(launcherTranslation, currentTarget.getPosition(), 10);
             launchAngle = ballisticSolution.getLaunchAngle();
             rotationAngle = ballisticSolution.getRotationAngle();
             launchVelocity = ballisticSolution.getLaunchVelocity();
         }
 
-        setShooterLaunchAngle(launchAngle);
-        setShooterRotationAngle(rotationAngle);
-        setShooterVelocity(launchVelocity);
+        setLaunchAngle(launchAngle);
+        setRotationAngle(rotationAngle);
+        setVelocity(launchVelocity);
 
         SmartDashboard.putString("Shooter state: ", wantedState.toString());
 
@@ -119,23 +116,34 @@ public class Shooter extends SubsystemBase implements ITestableSubsystem {
         SmartDashboard.putNumber("Launch Velocity: ", launchVelocity);
     }
 
-    private void setShooterVelocity(double wantedVelocity) {
-        double output = MathUtil.clamp(wantedVelocity, -12.0, 12.0);
-
-        SmartDashboard.putNumber("Shooter Voltage", output);
-
-        topLaunchMotor.setControl(voltageControl.withOutput(output));
-        bottomLaunchMotor.setControl(voltageControl.withOutput(output));
+    public void setWantedState(SHOOTER_STATE state) {
+        System.out.println("Setting Shooter to: "+state);
+        this.wantedState = state;
     }
 
-    private void setShooterLaunchAngle(double wantedAngle) {
-        double rotations = (wantedAngle / 360.0) * GEAR_RATIO;
+    public void setCurrentAutoAimTarget(AUTO_AIM_TARGETS target) {
+        this.currentTarget = target;
+    }
+
+    private void setVelocity(double wantedVelocity) {
+        double output = MathUtil.clamp(wantedVelocity, 0, 100);
+
+        topLaunchMotor.setControl(velocityControl.withVelocity(output));
+        bottomLaunchMotor.setControl(velocityControl.withVelocity(output));
+    }
+
+    private void setLaunchAngle(double wantedAngleDegrees) {
+        double rotations = wantedAngleDegrees * MOTOR_ROTATIONS_PER_LAUNCH_ANGLE_DEGREE;
+
+        double output = MathUtil.clamp(rotations, 0, 1000); //TODO WHEN PHYSICAL SUBSYSTEM EXISTS, set this.
 
         launchAngleMotor.setControl(positionControl.withPosition(rotations));
     }
 
-    private void setShooterRotationAngle(double wantedAngle) {
-        double rotations = (wantedAngle / 360.0) * GEAR_RATIO;
+    private void setRotationAngle(double wantedAngleDegrees) {
+        double rotations = wantedAngleDegrees * MOTOR_ROTATIONS_PER_ROTATION_ANGLE_DEGREE;
+
+        double output = MathUtil.clamp(rotations, -100, 100); //TODO WHEN PHYSICAL SUBSYSTEM EXISTS, set this.
 
         rotationAngleMotor.setControl(positionControl.withPosition(rotations));
     }
