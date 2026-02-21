fe3a30109c3e5d90bac4d2c1ef61fb00e5a0c764
ishwaqo8120 <ishmomar14@gmail.com>
2026-02-21 14:15:07 -0600

    Added quality of life changes  (spacing, comments, and removed unused lines)

--- src/main/java/com/team1816/season/subsystems/Superstructure.java
+++ src/main/java/com/team1816/season/subsystems/Superstructure.java
@@ -3,6 +3,7 @@ package com.team1816.season.subsystems;
 import com.team1816.lib.Singleton;
 import com.team1816.lib.subsystems.drivetrain.Swerve;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -19,63 +20,80 @@ public class Superstructure extends SubsystemBase {
     private CommandXboxController controller;
 
     public enum WantedSuperState {
-
         DEFAULT,
+
         SHOOTER_CALIBRATE,
+
         SHOOTER_AUTOMATIC_HUB,
         SHOOTER_AUTOMATIC_CORNER,
+
         SHOOTER_DISTANCE_1,
         SHOOTER_DISTANCE_2,
         SHOOTER_DISTANCE_3,
+
         SNOWBLOWER_AUTOMATIC_HUB,
         SNOWBLOWER_AUTOMATIC_CORNER,
+
         SNOWBLOWER_DISTANCE_1,
         SNOWBLOWER_DISTANCE_2,
         SNOWBLOWER_DISTANCE_3,
+
         INTAKE_LIFT,
         INTAKE_DROP,
+
         INTAKE,
         OUTTAKE,
+
         GATEKEEPER_ON,
         GATEKEEPER_OFF,
+
         CLIMB_L1,
         CLIMB_L3,
         CLIMB_DOWN_L1,
+
         FEEDER_SLOW,
         FEEDER_FAST,
         FEEDER_AGITATE,
+
         DROP_HEIGHT
-
-
-
     }
 
     public enum ActualSuperState {
-
         DEFAULTING,
+
         SHOOTING_CALIBRATING,
+
         SHOOTING_AUTOMATIC_HUB,
         SHOOTING_AUTOMATIC_CORNER,
+
         SHOOTING_DISTANCE_1,
         SHOOTING_DISTANCE_2,
         SHOOTING_DISTANCE_3,
+
         SNOWBLOWING_AUTOMATIC_HUB,
         SNOWBLOWING_AUTOMATIC_CORNER,
+
         SNOWBLOWING_DISTANCE_1,
         SNOWBLOWING_DISTANCE_2,
         SNOWBLOWING_DISTANCE_3,
+
         INTAKE_LIFTING,
         INTAKE_DROPPING,
+
         INTAKING,
         OUTTAKING,
+
         GATEKEEPING_ON,
         GATEKEEPING_OFF,
+
         CLIMBING_L1,
         CLIMBING_L3,
         CLIMBING_DOWN_L1,
+
         FEEDING_SLOW,
         FEEDING_FAST,
         FEEDING_AGITATING,
+
         DROPPING_HEIGHT
     }
 
@@ -98,13 +116,6 @@ public class Superstructure extends SubsystemBase {
     public enum WantedSwerveState {
         AUTOMATIC_DRIVING,
         MANUAL_DRIVING
-    }
-
-    public enum WantedFeederState {
-        SLOW_FEEDING,
-        FAST_FEEDING,
-        REVERSING,
-        IDLING
     }
 
     public enum FeederControlState {
@@ -144,34 +155,47 @@ public class Superstructure extends SubsystemBase {
         GreenLogger.log("Actual Superstate " + actualSuperState);
 
         applyStates();
+
+        SmartDashboard.putString("Super state: ", wantedSuperState.toString());
     }
 
     private ActualSuperState handleStateTransitions() {
         switch (wantedSuperState) {
             case DEFAULT -> actualSuperState = ActualSuperState.DEFAULTING;
+
             case SHOOTER_CALIBRATE -> actualSuperState = ActualSuperState.SHOOTING_CALIBRATING;
+
             case SHOOTER_AUTOMATIC_HUB -> actualSuperState = ActualSuperState.SHOOTING_AUTOMATIC_HUB;
             case SHOOTER_AUTOMATIC_CORNER -> actualSuperState = ActualSuperState.SHOOTING_AUTOMATIC_CORNER;
+
             case SHOOTER_DISTANCE_1 -> actualSuperState = ActualSuperState.SHOOTING_DISTANCE_1;
             case SHOOTER_DISTANCE_2 -> actualSuperState = ActualSuperState.SHOOTING_DISTANCE_2;
             case SHOOTER_DISTANCE_3 -> actualSuperState = ActualSuperState.SHOOTING_DISTANCE_3;
+
             case SNOWBLOWER_AUTOMATIC_HUB -> actualSuperState = ActualSuperState.SNOWBLOWING_AUTOMATIC_HUB;
             case SNOWBLOWER_AUTOMATIC_CORNER -> actualSuperState = ActualSuperState.SNOWBLOWING_AUTOMATIC_CORNER;
+
             case SNOWBLOWER_DISTANCE_1 -> actualSuperState = ActualSuperState.SNOWBLOWING_DISTANCE_1;
             case SNOWBLOWER_DISTANCE_2 -> actualSuperState = ActualSuperState.SNOWBLOWING_DISTANCE_2;
             case SNOWBLOWER_DISTANCE_3 -> actualSuperState = ActualSuperState.SNOWBLOWING_DISTANCE_3;
+
             case INTAKE_LIFT -> actualSuperState = ActualSuperState.INTAKE_LIFTING;
             case INTAKE_DROP -> actualSuperState = ActualSuperState.INTAKE_DROPPING;
+
             case INTAKE -> actualSuperState = ActualSuperState.INTAKING;
             case OUTTAKE -> actualSuperState = ActualSuperState.OUTTAKING;
+
             case GATEKEEPER_ON -> actualSuperState = ActualSuperState.GATEKEEPING_ON;
             case GATEKEEPER_OFF -> actualSuperState = ActualSuperState.GATEKEEPING_OFF;
+
             case CLIMB_L1 -> actualSuperState = ActualSuperState.CLIMBING_L1;
             case CLIMB_L3 -> actualSuperState = ActualSuperState.CLIMBING_L3;
             case CLIMB_DOWN_L1 -> actualSuperState = ActualSuperState.CLIMBING_DOWN_L1;
+
             case FEEDER_SLOW -> actualSuperState = ActualSuperState.FEEDING_SLOW;
             case FEEDER_FAST -> actualSuperState = ActualSuperState.FEEDING_FAST;
             case FEEDER_AGITATE -> actualSuperState = ActualSuperState.FEEDING_AGITATING;
+
             case DROP_HEIGHT -> actualSuperState = ActualSuperState.DROPPING_HEIGHT;
         }
 
@@ -182,29 +206,40 @@ public class Superstructure extends SubsystemBase {
     private void applyStates() {
         switch (actualSuperState) {
             case DEFAULTING -> defaulting();
+
             case SHOOTING_CALIBRATING -> shootCalibrating();
+
             case SHOOTING_AUTOMATIC_HUB -> shootingAutomaticHub();
             case SHOOTING_AUTOMATIC_CORNER -> shootingAutomaticCorner();
+
             case SHOOTING_DISTANCE_1 -> shootingDistance1();
             case SHOOTING_DISTANCE_2 -> shootingDistance2();
             case SHOOTING_DISTANCE_3 -> shootingDistance3();
+
             case SNOWBLOWING_AUTOMATIC_HUB -> snowblowingAutomaticHub();
             case SNOWBLOWING_AUTOMATIC_CORNER -> snowblowingAutomaticCorner();
+
             case SNOWBLOWING_DISTANCE_1 -> snowblowingDistance1();
             case SNOWBLOWING_DISTANCE_2 -> snowblowingDistance2();
             case SNOWBLOWING_DISTANCE_3 -> snowblowingDistance3();
+
             case INTAKE_LIFTING -> intakeLifting();
             case INTAKE_DROPPING -> intakeDropping();
+
             case INTAKING -> intaking();
             case OUTTAKING -> outtaking();
+
             case GATEKEEPING_ON -> gatekeepingOn();
             case GATEKEEPING_OFF -> gatekeeperingOff();
+
             case CLIMBING_L1 -> climbingL1();
             case CLIMBING_L3 -> climbingL3();
             case CLIMBING_DOWN_L1 -> climbingDownL1();
+
             case FEEDING_SLOW -> feedingSlow();
             case FEEDING_FAST -> feedingFast();
             case FEEDING_AGITATING -> agitate();
+
             case DROPPING_HEIGHT -> droppingHeight();
         }
     }
@@ -366,7 +401,7 @@ public class Superstructure extends SubsystemBase {
     }
 
     public void autonomousInit() {
-
+            //What is supposed to be here???
     }
 
     public void teleopInit() {
