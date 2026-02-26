fbd3cfb7a921bd1a78b42f0c8d37606b35a7c38b
ishwaqo8120 <ishmomar14@gmail.com>
2026-02-26 17:37:54 -0600

    Some re-naming + quality of life changes (spelling)

--- src/main/java/com/team1816/season/DuckingPerimeterManagement.java
+++ src/main/java/com/team1816/season/DuckingPerimeterManagement.java
@@ -4,7 +4,7 @@ import com.team1816.lib.BaseRobotState;
 import edu.wpi.first.math.geometry.Rectangle2d;
 import edu.wpi.first.math.geometry.Translation2d;
 
-public class PerimeterMath {
+public class DuckingPerimeterManagement {
     private final Translation2d upperLeftCorner = new Translation2d(0, 5); //Need to set
     private final Translation2d lowerRightCorner = new Translation2d(5,0);
     private final Rectangle2d perimeter = new Rectangle2d(upperLeftCorner, lowerRightCorner);
@@ -13,7 +13,9 @@ public class PerimeterMath {
     public boolean isInsidePerimeter() {
         return isInsidePerimeter;
     }
+
     public boolean isNotInsidePerimeter() {return !isInsidePerimeter;}
+
     public boolean checkIfInsidePerimeter() {
         if(perimeter.contains(new Translation2d(BaseRobotState.swerveDriveState.Pose.getX(), BaseRobotState.swerveDriveState.Pose.getY()))) {
             return isInsidePerimeter = true;
