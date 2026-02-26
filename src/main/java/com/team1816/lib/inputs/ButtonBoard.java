93d448f5779126d64c765eea436923fe0bc490c3
Erikbrovold1 <142361113+Erikbrovold1@users.noreply.github.com>
2026-02-25 18:59:34 -0600

    Added a button board unit test to confirm button board functionality while fixing errors in CommandButtonBoard

--- src/main/java/com/team1816/lib/inputs/ButtonBoard.java
+++ src/main/java/com/team1816/lib/inputs/ButtonBoard.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package com.team1816.lib.inputs;
 
 import edu.wpi.first.hal.FRCNetComm.tResourceType;
@@ -16,26 +12,26 @@ import edu.wpi.first.wpilibj.event.EventLoop;
 public class ButtonBoard extends GenericHID implements Sendable {
     // These values may be wrong
     public enum Button {
-        /** Top-left button. */
+        // Top-left button
         kTopLeft(2),
-        /** Top-center button. */
+        // Top-center button
         kTopCenter(5),
-        /** Top-right button. */
+        // Top-right button
         kTopRight(12),
-        /** Middle-left button. */
+        // Middle-left button
         kMiddleLeft(3),
-        /** Middle-center button. */
+        // Middle-center button
         kMiddleCenter(4),
-        /** Middle-right button. */
+        // Middle-right button
         kMiddleRight(10),
-        /** Bottom-left button. */
+        // Bottom-left button
         kBottomLeft(9),
-        /** Bottom-center button. */
+        // Bottom-center button
         kBottomCenter(11),
-        /** Bottom-right button. */
+        // Bottom-right button
         kBottomRight(13);
 
-        /** Button value. */
+        // Button value.
         public final int value;
 
         Button(int value) {
@@ -58,15 +54,12 @@ public class ButtonBoard extends GenericHID implements Sendable {
     public boolean getTopLeftButton() {
         return getRawButton(Button.kTopLeft.value);
     }
-
     public boolean getTopLeftButtonPressed() {
         return getRawButtonPressed(Button.kTopLeft.value);
     }
-
     public boolean getTopLeftButtonReleased() {
         return getRawButtonReleased(Button.kTopLeft.value);
     }
-
     public BooleanEvent topLeft(EventLoop loop) {
         return button(Button.kTopLeft.value, loop);
     }
@@ -75,15 +68,12 @@ public class ButtonBoard extends GenericHID implements Sendable {
     public boolean getTopCenterButton() {
         return getRawButton(Button.kTopCenter.value);
     }
-
     public boolean getTopCenterButtonPressed() {
         return getRawButtonPressed(Button.kTopCenter.value);
     }
-
     public boolean getTopCenterButtonReleased() {
         return getRawButtonReleased(Button.kTopCenter.value);
     }
-
     public BooleanEvent topCenter(EventLoop loop) {
         return button(Button.kTopCenter.value, loop);
     }
@@ -92,15 +82,12 @@ public class ButtonBoard extends GenericHID implements Sendable {
     public boolean getTopRightButton() {
         return getRawButton(Button.kTopRight.value);
     }
-
     public boolean getTopRightButtonPressed() {
         return getRawButtonPressed(Button.kTopRight.value);
     }
-
     public boolean getTopRightButtonReleased() {
         return getRawButtonReleased(Button.kTopRight.value);
     }
-
     public BooleanEvent topRight(EventLoop loop) {
         return button(Button.kTopRight.value, loop);
     }
@@ -109,15 +96,12 @@ public class ButtonBoard extends GenericHID implements Sendable {
     public boolean getMiddleLeftButton() {
         return getRawButton(Button.kMiddleLeft.value);
     }
-
     public boolean getMiddleLeftButtonPressed() {
         return getRawButtonPressed(Button.kMiddleLeft.value);
     }
-
     public boolean getMiddleLeftButtonReleased() {
         return getRawButtonReleased(Button.kMiddleLeft.value);
     }
-
     public BooleanEvent middleLeft(EventLoop loop) {
         return button(Button.kMiddleLeft.value, loop);
     }
@@ -126,15 +110,12 @@ public class ButtonBoard extends GenericHID implements Sendable {
     public boolean getMiddleCenterButton() {
         return getRawButton(Button.kMiddleCenter.value);
     }
-
     public boolean getMiddleCenterButtonPressed() {
         return getRawButtonPressed(Button.kMiddleCenter.value);
     }
-
     public boolean getMiddleCenterButtonReleased() {
         return getRawButtonReleased(Button.kMiddleCenter.value);
     }
-
     public BooleanEvent middleCenter(EventLoop loop) {
         return button(Button.kMiddleCenter.value, loop);
     }
@@ -143,52 +124,40 @@ public class ButtonBoard extends GenericHID implements Sendable {
     public boolean getMiddleRightButton() {
         return getRawButton(Button.kMiddleRight.value);
     }
-
     public boolean getMiddleRightButtonPressed() {
         return getRawButtonPressed(Button.kMiddleRight.value);
     }
-
     public boolean getMiddleRightButtonReleased() {
         return getRawButtonReleased(Button.kMiddleRight.value);
     }
-
     public BooleanEvent middleRight(EventLoop loop) {
         return button(Button.kMiddleRight.value, loop);
     }
 
 
-
     public boolean getBottomLeftButton() {
         return getRawButton(Button.kBottomLeft.value);
     }
-
     public boolean getBottomLeftButtonPressed() {
         return getRawButtonPressed(Button.kBottomLeft.value);
     }
-
     public boolean getBottomLeftButtonReleased() {
         return getRawButtonReleased(Button.kBottomLeft.value);
     }
-
     public BooleanEvent bottomLeft(EventLoop loop) {
         return button(Button.kBottomLeft.value, loop);
     }
 
+
     public boolean getBottomCenterButton() {
         return getRawButton(Button.kBottomCenter.value);
     }
-
-
     public boolean getBottomCenterButtonPressed() {
         return getRawButtonPressed(Button.kBottomCenter.value);
     }
-
-
     public boolean getBottomCenterButtonReleased() {
         return getRawButtonReleased(Button.kBottomCenter.value);
     }
-
-
     public BooleanEvent bottomCenter(EventLoop loop) {
         return button(Button.kBottomCenter.value, loop);
     }
@@ -197,31 +166,29 @@ public class ButtonBoard extends GenericHID implements Sendable {
     public boolean getBottomRightButton() {
         return getRawButton(Button.kBottomRight.value);
     }
-
     public boolean getBottomRightButtonPressed() {
         return getRawButtonPressed(Button.kBottomRight.value);
     }
-
     public boolean getBottomRightButtonReleased() {
         return getRawButtonReleased(Button.kBottomRight.value);
     }
-
     public BooleanEvent bottomRight(EventLoop loop) {
         return button(Button.kBottomRight.value, loop);
     }
 
+    //Lets dashboard get buttons but not set them
     @Override
     public void initSendable(SendableBuilder builder) {
         builder.setSmartDashboardType("HID");
         builder.publishConstString("ControllerType", "ButtonBoard");
-        builder.addBooleanProperty("TopLeft",      this::getTopLeftButton,     null);
-        builder.addBooleanProperty("TopCenter",    this::getTopCenterButton,   null);
-        builder.addBooleanProperty("TopRight",     this::getTopRightButton,    null);
-        builder.addBooleanProperty("MiddleLeft",   this::getMiddleLeftButton,  null);
+        builder.addBooleanProperty("TopLeft",      this::getTopLeftButton,      null);
+        builder.addBooleanProperty("TopCenter",    this::getTopCenterButton,    null);
+        builder.addBooleanProperty("TopRight",     this::getTopRightButton,     null);
+        builder.addBooleanProperty("MiddleLeft",   this::getMiddleLeftButton,   null);
         builder.addBooleanProperty("MiddleCenter", this::getMiddleCenterButton, null);
-        builder.addBooleanProperty("MiddleRight",  this::getMiddleRightButton, null);
-        builder.addBooleanProperty("BottomLeft",   this::getBottomLeftButton,  null);
+        builder.addBooleanProperty("MiddleRight",  this::getMiddleRightButton,  null);
+        builder.addBooleanProperty("BottomLeft",   this::getBottomLeftButton,   null);
         builder.addBooleanProperty("BottomCenter", this::getBottomCenterButton, null);
-        builder.addBooleanProperty("BottomRight",  this::getBottomRightButton, null);
+        builder.addBooleanProperty("BottomRight",  this::getBottomRightButton,  null);
     }
 }
