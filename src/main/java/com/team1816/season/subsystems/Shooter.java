f479f1f40e1157147ac101dd7c6b3d3389b86293
clarkmilbert <clarkmilbert@gmail.com>
2026-03-11 20:11:30 -0500

    Re-added check for robot crashing and removed GreenLogger usages that were for testing

--- src/main/java/com/team1816/season/subsystems/Shooter.java
+++ src/main/java/com/team1816/season/subsystems/Shooter.java
@@ -176,10 +176,9 @@ public class Shooter extends SubsystemBase implements ITestableSubsystem {
     public void periodic() {
         readFromHardware();
 
-        // TODO: Uncomment this once we add automatic calibration.
-//        if (rotationAngleMotor.hasDeviceCrashed()) {
-//            isCalibrated = false;
-//        }
+        if (rotationAngleMotor.hasDeviceCrashed()) {
+            isCalibrated = false;
+        }
 
         if (!isCalibrated) {
             calibratePeriodic();
@@ -319,26 +318,22 @@ public class Shooter extends SubsystemBase implements ITestableSubsystem {
             // Set the initial stalling timestamp if it hasn't been set yet
             if (initialCalibrationStallingTimestamp == -1 && !rotationAngleSensorClockwiseLeft.get() && !rotationAngleSensorClockwiseRight.get()) {
                 initialCalibrationStallingTimestamp = Timer.getFPGATimestamp();
-                GreenLogger.log("Reset timestamp");
             }
 
             // If the timestamp has been initialized and the time elapsed has reached the calibration stalling seconds...
             if (initialCalibrationStallingTimestamp != -1 && Timer.getFPGATimestamp() - initialCalibrationStallingTimestamp >= CALIBRATION_STALL_SECONDS) {
                 // Move clockwise slowly
                 rotationAngleMotor.setControl(turretDutyCycleOutRequest.withOutput(-SLOW_CALIBRATION_SPEED));
-                GreenLogger.log("Stalled, going backwards");
             }
             // Otherwise, if the right beam break is tripped...
             else if (!rotationAngleSensorClockwiseRight.get()) {
                 // Move counterclockwise slowly
                 rotationAngleMotor.setControl(turretDutyCycleOutRequest.withOutput(SLOW_CALIBRATION_SPEED));
-                GreenLogger.log("Right sensor is hit, going slowly");
             }
             // Otherwise (so if right sensor isn't tripped, and either the initial calibration timestamp hasn't been initialized or it hasn't reached the stalling time)...
             else {
                 // Move counterclockwise quickly
                 rotationAngleMotor.setControl(turretDutyCycleOutRequest.withOutput(FAST_CALIBRATION_SPEED));
-                GreenLogger.log("Right sensor not hit, going quickly");
             }
         }
         // Reset the initial calibration timestamp in case the robot is ever re-enabled
