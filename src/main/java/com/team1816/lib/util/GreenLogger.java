bac2645bfa15ccd8e2fa634971a7a5954288574d
David Day <dcday@themilwaukeeroad.com>
2026-04-09 21:54:33 -0500

    reset logLoopCount when disabled to avoid unnecessary logging

--- src/main/java/com/team1816/lib/util/GreenLogger.java
+++ src/main/java/com/team1816/lib/util/GreenLogger.java
@@ -6,13 +6,9 @@ import com.pathplanner.lib.config.RobotConfig;
 import com.team1816.season.Robot;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.networktables.*;
-import edu.wpi.first.util.datalog.DataLog;
 import edu.wpi.first.util.struct.Struct;
 import edu.wpi.first.util.struct.StructSerializable;
-import edu.wpi.first.wpilibj.DataLogManager;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.IterativeRobotBase;
-import edu.wpi.first.wpilibj.Watchdog;
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -339,6 +335,7 @@ public class GreenLogger {
     // Will update all registered periodic loggers
     @SuppressWarnings({"unchecked", "rawtypes"})
     public static void updatePeriodic() {
+        if(RobotState.isDisabled()) { logLoopCount = 0; return; }
         if (logLoopCount <= 2) {
             logLoopCount ++;
             return;
