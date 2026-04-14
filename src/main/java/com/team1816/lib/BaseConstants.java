bacd80563512e7021054a5130d6a9e1e300efe77
henrymilbert <110063972+henrymilbert@users.noreply.github.com>
2026-04-13 22:44:01 -0500

    figure out how to load a custom tag layout from json

--- src/main/java/com/team1816/lib/BaseConstants.java
+++ src/main/java/com/team1816/lib/BaseConstants.java
@@ -9,16 +9,44 @@ import edu.wpi.first.math.numbers.N3;
 
 public final class BaseConstants {
     public static final class VisionConstants {
-        public static final AprilTagFieldLayout aprilTagFieldLayout =
-//            new AprilTagFieldLayout(
-//                List.of(
-//                    new AprilTag(15, new Pose3d(
-//                        new Translation3d(3, 3, Units.inchesToMeters(24)),
-//                        new Rotation3d(0, 0, Math.PI)
-//                    ))
-//                ), 16.48, 8.10
-//            );
-            AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+        // Use this to load the default AprilTag field layout for the season. This will probably
+        // be the welded version of the field. This can also be used to load the AndyMark field or
+        // fields from previous seasons by changing the kDefaultField to the desired field.
+        public static final AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(
+            AprilTagFields.kDefaultField
+        );
+
+        // Use this to use a custom AprilTag field layout specified as a list of AprilTags with
+        // their Pose3ds.
+//        public static final AprilTagFieldLayout aprilTagFieldLayout = new AprilTagFieldLayout(
+//            List.of(
+//                new AprilTag(15, new Pose3d(
+//                    new Translation3d(3, 3, Units.inchesToMeters(24)),
+//                    new Rotation3d(0, 0, Math.PI)
+//                ))
+//            ), 16.48, 8.10
+//        );
+
+        // Use this to load a custom AprilTag field layout from a JSON file in the deploy
+        // directory. This can be useful for using a field layout calibrated with WPIcal.
+//        public static final AprilTagFieldLayout aprilTagFieldLayout;
+//        static {
+//            AprilTagFieldLayout layout;
+//            try {
+//                Path layoutPath = Filesystem.getDeployDirectory().toPath().resolve("tag_layouts/testing-layout.json");
+//                layout = new AprilTagFieldLayout(layoutPath);
+//            } catch (IOException e) {
+//                // If there was a problem loading the field layout (likely the file path was
+//                // specified incorrectly), fall back to the default field.
+//                layout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+//                GreenLogger.log(e);
+//                GreenLogger.log(
+//                    "IOException while loading AprilTag field layout. See above stack trace for " +
+//                        "details."
+//                );
+//            }
+//            aprilTagFieldLayout = layout;
+//        }
     }
 
     public static final class DrivetrainConstants {
